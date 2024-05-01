8d24810f3d33aec35688faa5632d94eae7de46ff
Andreyka <Andreyka.work.acc@gmail.com>
2024-05-01 20:12:17 +0700

    chore: delete coffee maker code

--- public/index.php
+++ public/index.php
@@ -2,25 +2,4 @@
 
 declare(strict_types=1);
 
-use App\AllInOneCoffeeMaker;
-use App\CappuccinoMaker;
-use App\CoffeeMaker;
-use App\LatteMaker;
-
 require __DIR__ . '/../vendor/autoload.php';
-
-$coffeeMaker = new CoffeeMaker();
-$coffeeMaker->makeCoffee();
-
-$latteMaker = new LatteMaker();
-$latteMaker->makeCoffee();
-$latteMaker->makeLatte();
-
-$cappuccinoMaker = new CappuccinoMaker();
-$cappuccinoMaker->makeCoffee();
-$cappuccinoMaker->makeCappuccino();
-
-$allInOnCoffeeMaker = new AllInOneCoffeeMaker();
-$allInOnCoffeeMaker->makeCoffee();
-$allInOnCoffeeMaker->makeLatte();
-$allInOnCoffeeMaker->makeCappuccino();
